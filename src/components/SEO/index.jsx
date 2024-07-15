6190ef792da6d54db63dc2b0402562f073d04a31
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-16 03:44:07 +0900

    [env] 구글애드센스 추가

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -9,7 +9,6 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
-      <meta name="google-adsense-account" content="ca-pub-6244749261128518" />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
